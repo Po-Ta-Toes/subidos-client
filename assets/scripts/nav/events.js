f9efa568832bb43c47433dc67cc24a66f4bcf174
srsexton94 <srsexton94@gmail.com>
2020-05-24 14:49:25 -0400

    Add open nav functionality to make sidenav toggle

--- assets/scripts/nav/events.js
+++ assets/scripts/nav/events.js
@@ -13,12 +13,20 @@ const toSignUp = function () {
 const closeNav = function () {
   $('.side-nav').addClass('hidden')
   $('.log-section').css('left', '0')
+  $('.open').removeClass('hidden')
+}
+
+const openNav = function () {
+  $('.side-nav').removeClass('hidden')
+  $('.log-section').css('left', '23rem')
+  $('.open').addClass('hidden')
 }
 
 const addHandlers = function () {
   $('.signin-nav').on('click', toSignIn)
   $('.signup-nav').on('click', toSignUp)
   $('.main-content').on('click', '.close', closeNav)
+  $('.main-content').on('click', '.open', openNav)
 }
 
 module.exports = {
